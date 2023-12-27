09bc89b0cf9742d6748ce4c0e0ea8791a8ad4fbe
mauriciorbrito <maurodbri@gmail.com>
2023-12-27 15:58:18 -0500

    se agrega un . para probar análisis en sona cloud

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
-		return "Hola, soy Mauricio Brito";
+		return "Hola, soy Mauricio Brito.";
 	}
 }
